824fa0dbb49468a7196594589e0cad94915bdadb
Takuya Takahashi <74673754+Takahat@users.noreply.github.com>
2025-06-01 19:05:36 +0900

    Bootstrapで変更

--- assessment.js
+++ assessment.js
@@ -15,14 +15,28 @@ assessmentButton.addEventListener(
 
     // 診断結果エリアの作成
     resultDivision.innerText = ''; // 前の診断結果を消す
-    const header = document.createElement('h3');
-    header.innerText = '診断結果';
-    resultDivision.appendChild(header);
+
+    // headerDivisionの作成    
+    const headerDivision = document.createElement('div');
+    headerDivision.setAttribute('class', 'card-header text-bg-primary');
+    headerDivision.innerText = '診断結果';
+
+    // bodyDivisionの作成
+    const bodyDivision = document.createElement('div');
+    bodyDivision.setAttribute('class', 'card-body');
 
     const paragraph = document.createElement('p');
+    paragraph.setAttribute('class', 'card-text');
     const result = assessment(userName);
     paragraph.innerText = result;
-    resultDivision.appendChild(paragraph);
+    bodyDivision.appendChild(paragraph);
+
+    // resultDivisionにBootstrapのスタイルを適用する
+    resultDivision.setAttribute('class', 'card');
+
+    // headerDivision と bodyDivisionを resultDivisionに差し込む
+    resultDivision.appendChild(headerDivision);
+    resultDivision.appendChild(bodyDivision);
 
     // ツイートエリアの作成
     tweetDivision.innerText = ''; // 前のツイートエリアを消す
